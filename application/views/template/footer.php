64fcc0a57f8346f68d2880e4f5d605305f12dfea
risarahayu <rrisa329@gmail.com>
2024-07-16 10:48:36 +0800

    install template

---
+++ application/views/template/footer.php
@@ -0,0 +1,28 @@
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- /#page-wrapper -->
+
+        </div>
+        <!-- /#wrapper -->
+
+        <!-- jQuery -->
+        <script src="<?=BASEURL ?>asset/js/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="<?=BASEURL ?>asset/js/bootstrap.min.js"></script>
+
+        <!-- Metis Menu Plugin JavaScript -->
+        <script src="<?=BASEURL ?>asset/js/metisMenu.min.js"></script>
+
+        <!-- Morris Charts JavaScript -->
+        <script src="<?=BASEURL ?>asset/js/raphael.min.js"></script>
+        <script src="<?=BASEURL ?>asset/js/morris.min.js"></script>
+        <script src="<?=BASEURL ?>asset/js/morris-data.js"></script>
+
+        <!-- Custom Theme JavaScript -->
+        <script src="<?=BASEURL ?>asset/js/startmin.js"></script>
+
+    </body>
+
+</html>
