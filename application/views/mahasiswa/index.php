5670ec08f05528dccd84301c25a4ab0e2c0fa98d
risarahayu <rrisa329@gmail.com>
2024-07-16 13:44:59 +0800

    add button add data in mahasiswa index

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -6,6 +6,7 @@
                                 </div>
                                 <!-- /.panel-heading -->
                                 <div class="panel-body">
+                                    <a href="" class="btn btn-primary" style="margin-bottom:5px;" type="button">Add data</a>
                                     <div class="table-responsive">
                                         <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                                             <thead>
