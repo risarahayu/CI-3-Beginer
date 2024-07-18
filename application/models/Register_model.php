0c3057fa6cf7b490e8910bb0dcec03b61252f624
risarahayu <rrisa329@gmail.com>
2024-07-18 12:04:36 +0800

    modif model register

---
+++ application/models/Register_model.php
@@ -0,0 +1,14 @@
+<?php
+class Register_model extends CI_Model{
+    public function register($data){
+        // karena kita kirim dengan methos post, jadi kita simpan ke dalam $data
+        $nama=$data['nama'];
+        $email=$data['email'];
+        $username=$data['username'];
+        $password=md5($data['password']);
+
+        // echo $password;die();
+
+        return $this->db->query("INSERT INTO users VALUES('','$nama', '$email', '$username', '$password')");
+    }
+}
